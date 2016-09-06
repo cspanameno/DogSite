d376fc14d5c0d787775630235faf5a1c55408b02
Cindy Panameno <cspanameno2013@gmail.com>
2016-09-06 08:58:19 -0700

    added styling

--- pet_seed.py
+++ pet_seed.py
@@ -52,50 +52,6 @@ def load_breeds():
     db.session.commit()
 
 
-
-
-
-
-
-# def load_ratings():
-#     """Load ratings from u.data into database."""
-
-#     print "Ratings"
-
-#     for i, row in enumerate(open("seed_data/u.data")):
-#         row = row.rstrip()
-
-#         user_id, movie_id, score, timestamp = row.split("\t")
-
-#         user_id = int(user_id)
-#         movie_id = int(movie_id)
-#         score = int(score)
-
-#         # We don't care about the timestamp, so we'll ignore this
-
-#         rating = Rating(user_id=user_id,
-#                         movie_id=movie_id,
-#                         score=score)
-
-#         # We need to add to the session or it won't ever be stored
-#         db.session.add(rating)
-
-#         # provide some sense of progress
-#         if i % 1000 == 0:
-#             print i
-
-#             # An optimization: if we commit after every add, the database
-#             # will do a lot of work committing each record. However, if we
-#             # wait until the end, on computers with smaller amounts of
-#             # memory, it might thrash around. By committing every 1,000th
-#             # add, we'll strike a good balance.
-
-#             db.session.commit()
-
-#     # Once we're done, we should commit our work
-#     db.session.commit()
-
-
 def set_val_user_id():
     """Set value for the next user_id after seeding database"""
 
